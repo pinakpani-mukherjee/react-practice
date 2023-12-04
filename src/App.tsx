0a423a713dc1f418654fb4c530566a922ef5dbb7
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2023-12-04 20:49:44 +0900

    added button and react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
 import TestButton from "./components/TestButton";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   const handleButtonPress = (text: string) => {
     console.log(text);
+    setAlertVisibility(true);
+  };
+
+  const handleDismissAlert = () => {
+    setAlertVisibility(false);
   };
   return (
     <div>
-      <Alert>
-        <span>Hello World</span>
-      </Alert>
+      {alertVisible && <Alert onClose={handleDismissAlert}>My Alert</Alert>}
       <TestButton text="Press Me" onButtonPressed={handleButtonPress} />
     </div>
   );
