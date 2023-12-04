4711b489e54aca40a421e0ee080e6ee0bc64f286
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2023-12-04 17:32:35 +0900

    practicing state management

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
+
 function ListGroup() {
   const items: string[] = ["NYC", "San Fransisco", "Tokyo", "London", "Mumbai"];
 
-  //Event Handler
-  const handleClick = (event: React.MouseEvent) => console.log(event);
+  //Hook, allows us to tap into built in functions
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
