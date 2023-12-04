5564e109a781c16cba52aa29a78b3f9159c56dc3
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2023-12-04 20:53:06 +0900

    added icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import Alert from "./components/Alert";
-import TestButton from "./components/TestButton";
+import { BsFillCalendarFill } from "react-icons/bs";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-
-  const handleButtonPress = (text: string) => {
-    console.log(text);
-    setAlertVisibility(true);
-  };
-
-  const handleDismissAlert = () => {
-    setAlertVisibility(false);
-  };
   return (
     <div>
-      {alertVisible && <Alert onClose={handleDismissAlert}>My Alert</Alert>}
-      <TestButton text="Press Me" onButtonPressed={handleButtonPress} />
+      <BsFillCalendarFill color="blue" size="40" />
     </div>
   );
 }
